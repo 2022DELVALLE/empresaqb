eb1bf37fe869327e06e8dfbf2bf23541b83cf1b7
kenyo <delalleyo@gmail.com>
2023-05-02 22:06:22 -0500

    salvar usuario

--- src/containers/S1_Navbar.jsx
+++ src/containers/S1_Navbar.jsx
@@ -152,132 +152,138 @@ const S1_Navbar = () => {
     //Control del nombre inicio sesion por el nombre de usuario
     const { user } = React.useContext(AppContext); 
 
-    return (
-        <>
-            <header>
-                <nav className={portalMenuP ? 'navbar ' : 'navbar prioridad'} id="nav-bar">
-                    <div className="navbar__logo"
-                        onClick={() => hanldeToHome()}
-                    >
-                        <a>
-                            <span> EMPRESA</span>
-                        </a>
-                        <a >
-                            <img src={logoBackgroundWhite} alt="Logo Ilusion QB" />
-                        </a>
-                    </div>
-                    <div className="navbar__menucategories">
-                        <div className="container-fluid">
-                            <div
-                                onClick={() => handleClickMenuP()}
-                            >
-                                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                                    data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                                    <i className="fa-solid fa-bars iconmenu"></i>
-                                </button>
-                                <a className="navbar__menucategory-name">Menú</a>
-                            </div>
-                            <div className="offcanvas offcanvas-end" id="offcanvasNavbar"
-                                aria-labelledby="offcanvasNavbarLabel">
-                                <div className="offcanvas-header">
-                                    <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Categorias</h5>
-                                    <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas"
-                                        aria-label="Close">
+    if (!user) {
+    
+    } else {
+        return (
+            <>
+                <header>
+                    <nav className={portalMenuP ? 'navbar ' : 'navbar prioridad'} id="nav-bar">
+                        <div className="navbar__logo"
+                            onClick={() => hanldeToHome()}
+                        >
+                            <a>
+                                <span> EMPRESA</span>
+                            </a>
+                            <a >
+                                <img src={logoBackgroundWhite} alt="Logo Ilusion QB" />
+                            </a>
+                        </div>
+                        <div className="navbar__menucategories">
+                            <div className="container-fluid">
+                                <div
+                                    onClick={() => handleClickMenuP()}
+                                >
+                                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                                        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                                        <i className="fa-solid fa-bars iconmenu"></i>
                                     </button>
+                                    <a className="navbar__menucategory-name">Menú</a>
                                 </div>
-                                <div className="offcanvas-body">
-                                    <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                        <li className="nav-item">
-                                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                                        </li>
-                                        <li className="nav-item"><a className="nav-link" href="#">Accesorios de Madera</a></li>
-                                        <li className="nav-item"><a className="nav-link" href="#">Cuadernos personalizados</a></li>
-                                        <li className="nav-item"><a className="nav-link" href="#">Poleras personalizadas</a></li>
-                                        <li className="nav-item"><a className="nav-link" href="#">Recordatorio en madera</a></li>
-                                        <li className="nav-item"><a className="nav-link" href="#">Llaveros en madera</a></li>
-                                        <li className="nav-item"><a className="nav-link" href="#">Casacas personalizadas</a></li>
-                                        <li className="nav-item"><a className="nav-link" href="#">Peluches personalizados</a></li>
-                                        <li className="nav-item"><a className="nav-link" href="#">Popsocket</a></li>
-                                        <li className="nav-item"><a className="nav-link" href="#">Llaveros destacados</a></li>
-                                        <li className="nav-item"><a className="nav-link" href="#">Tazas personalizadas</a></li>
-                                    </ul>
+                                <div className="offcanvas offcanvas-end" id="offcanvasNavbar"
+                                    aria-labelledby="offcanvasNavbarLabel">
+                                    <div className="offcanvas-header">
+                                        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Categorias</h5>
+                                        <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+                                            aria-label="Close">
+                                        </button>
+                                    </div>
+                                    <div className="offcanvas-body">
+                                        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                            <li className="nav-item">
+                                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                            </li>
+                                            <li className="nav-item"><a className="nav-link" href="#">Accesorios de Madera</a></li>
+                                            <li className="nav-item"><a className="nav-link" href="#">Cuadernos personalizados</a></li>
+                                            <li className="nav-item"><a className="nav-link" href="#">Poleras personalizadas</a></li>
+                                            <li className="nav-item"><a className="nav-link" href="#">Recordatorio en madera</a></li>
+                                            <li className="nav-item"><a className="nav-link" href="#">Llaveros en madera</a></li>
+                                            <li className="nav-item"><a className="nav-link" href="#">Casacas personalizadas</a></li>
+                                            <li className="nav-item"><a className="nav-link" href="#">Peluches personalizados</a></li>
+                                            <li className="nav-item"><a className="nav-link" href="#">Popsocket</a></li>
+                                            <li className="nav-item"><a className="nav-link" href="#">Llaveros destacados</a></li>
+                                            <li className="nav-item"><a className="nav-link" href="#">Tazas personalizadas</a></li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-
-
-                    <div className="navbar__search">
-                        <div className="search_input">
-                            <input type="text" id="search"
-                                name="search" placeholder="Buscar productos"
-                                onChange={onChangeWordSearch}
-                                value={wordSearch}
-                                onKeyDown={searchProductsbyWordSearchonCkickDown}
-                            />
+    
+    
+                        <div className="navbar__search">
+                            <div className="search_input">
+                                <input type="text" id="search"
+                                    name="search" placeholder="Buscar productos"
+                                    onChange={onChangeWordSearch}
+                                    value={wordSearch}
+                                    onKeyDown={searchProductsbyWordSearchonCkickDown}
+                                />
+                            </div>
+                            <div className="seach_icon"
+                                onClick={searchProductsbyWordSearch}
+                            ><i className="bi bi-search"></i>
+                            </div>
                         </div>
-                        <div className="seach_icon"
-                            onClick={searchProductsbyWordSearch}
-                        ><i className="bi bi-search"></i>
+    
+                        <div className="navbar__login"
+                            onClick={() => handleButtonClick()}
+                        >
+                            <i className="bi bi-person-circle login-icon"></i>
+                            <div className="text-login">
+                                <a href="#" value="" >Hola,<br />{user ? user.data.user.name.split(" ").slice(0,2).join(" ") + '!' : 'inicia sesión!'}</a>
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="navbar__login"
-                        onClick={() => handleButtonClick()}
-                    >
-                        <i className="bi bi-person-circle login-icon"></i>
-                        <div className="text-login">
-                            <a href="#" value="" >Hola,<br />{user ? user.data.user.name.split(" ").slice(0,2).join(" ") + '!' : 'inicia sesión!'}</a>
+                        <div className="navbar__shop"
+                            onClick={hanldeToCart}
+                        >
+                            <a href="#"><i className="bi bi-cart"></i></a>
+                            {stateCart.cart.length > 0 ? <div>{stateCart.cart.length}</div> : null}
+    
                         </div>
-                    </div>
-                    <div className="navbar__shop"
-                        onClick={hanldeToCart}
-                    >
-                        <a href="#"><i className="bi bi-cart"></i></a>
-                        {stateCart.cart.length > 0 ? <div>{stateCart.cart.length}</div> : null}
+    
+    
+                    </nav>
+                </header>
+    
+                {/*Agrego la funcionalidad del Menu con un portal*/}
+    
+                {
+                    showPortal && (
+                        <PortalMenuInicio
+                            portalRef={portalRef} />
+                    )
+                }
+    
+                {/*Agrego la funcionalidad del Menu con un portal principal*/}
+    
+                {
+                    portalMenuP && (
+                        <PortalMenuPrincipal
+                            portalRefMenuP={portalRefMenuP}
+                            setPortalMenuP={setPortalMenuP}
+                        />
+                    )
+                }
+    
+    
+                {
+                    portalLogin && (
+                        <Login
+                            portalRefLogin={portalRefLogin}
+                        />
+                    )
+    
+                }
+    
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+                    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+                ></script>
+                {/*Revisar crossing anomimus*/}
+            </>
+        );
+    }
 
-                    </div>
 
-
-                </nav>
-            </header>
-
-            {/*Agrego la funcionalidad del Menu con un portal*/}
-
-            {
-                showPortal && (
-                    <PortalMenuInicio
-                        portalRef={portalRef} />
-                )
-            }
-
-            {/*Agrego la funcionalidad del Menu con un portal principal*/}
-
-            {
-                portalMenuP && (
-                    <PortalMenuPrincipal
-                        portalRefMenuP={portalRefMenuP}
-                        setPortalMenuP={setPortalMenuP}
-                    />
-                )
-            }
-
-
-            {
-                portalLogin && (
-                    <Login
-                        portalRefLogin={portalRefLogin}
-                    />
-                )
-
-            }
-
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-                integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-            ></script>
-            {/*Revisar crossing anomimus*/}
-        </>
-    );
 }
 
 export default S1_Navbar;
